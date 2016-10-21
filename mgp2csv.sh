a3f0b57f2cb09272439e1e9b86934e04a0414da2
Vishal Verma <vishal@kernel.org>
2016-10-21 01:49:13 -0600

    mgp2csv: add heat info parsing
    Public pages didn't have heat information. Add the ability to login
    using curl and saved cookies, and get to the logged-in view of the race,
    and parse pilors by heat, and group them.
    
    Also add some empty rows at the bottom as scribble space.
    
    Authentication is done using an 'auth' file in the current working
    directory. See auth.sample on how to fill it in.

--- mgp2csv.sh
+++ mgp2csv.sh
@@ -2,6 +2,16 @@
 
 base_url="http://www.multigp.com/races/view"
 dump="./race_file_raw"
+authfile="./auth"
+login_url="http://www.multigp.com/user/site/login"
+
+cleanup()
+{
+	[ -f cjar ] && rm -f cjar
+	[ -f convert.php ] && rm -f convert.php
+	#[ -f $dump ] && rm -f $dump
+	[ -f race.html ] && rm -f race.html
+}
 
 warn()
 {
@@ -16,6 +26,7 @@ die()
 	*) st=$2;;
 	esac
 	warn "$1"
+	cleanup
 	exit "$st"
 }
 
@@ -23,6 +34,16 @@ write_csv_headers()
 {
 	echo "MultiGP Race $race,$url" > $csv
 	echo "Name,Freq,Round 1,Round 2,Round 3,Round 4,Round 5,Round 6,Round 7,Round 8,Round 9,Final Points" >> $csv
+}
+
+write_csv_footer()
+{
+	echo "Misc" >> $csv
+	echo "A,,,,,,,,,,," >> $csv
+	echo "B,,,,,,,,,,," >> $csv
+	echo "C,,,,,,,,,,," >> $csv
+	echo "D,,,,,,,,,,," >> $csv
+	echo "E,,,,,,,,,,," >> $csv
 }
 
 [ -n "$1" ] || die "Please provide a race number or URL"
@@ -44,10 +65,9 @@ case "$1" in
 esac
 csv="./$race.csv"
 
-w3m -dump "$url" > $dump
-write_csv_headers
-
 freq_regex="[0-9]{4}\ [A-Z]\ [0-9]"
+pilot_regex="[0-9]+..[A-Z].[0-9]{4}\ (.*)"
+
 parse=''
 capture_freq=''
 buf=''
@@ -64,59 +84,124 @@ do_end()
 	buf=''
 }
 
-while IFS= read -r line; do
-	[ -z "$line" ] && continue
-	[[ "$line" == "Pilots Racing" ]] && parse=1 && continue # start parsing
-	[[ "$line" == "Comments" ]] && parse='' # stop when 'Comments' reached
-	[[ "$line" == "Leaderboard"* ]] && continue
-	[ -n "$parse" ] || continue
-	echo $line
+do_public_mode()
+{
+	w3m -dump "$url" > $dump
+	write_csv_headers
 
-	if [ $skip -gt 0 ]; then
-		((skip-=1))
-		continue
-	fi
+	# main parsing loop
+	while IFS= read -r line; do
+		[ -z "$line" ] && continue
+		[[ "$line" == "Pilots Racing" ]] && parse=1 && continue # start parsing
+		[[ "$line" == "Comments" ]] && parse='' # stop when 'Comments' reached
+		[[ "$line" == "Leaderboard"* ]] && continue
+		[ -n "$parse" ] || continue
+		echo $line
 
-	if [ $started -eq 0 ]; then
-		buf="$line"
-		((started+=1))
-	else
-		if [ -n "$capture_freq" ]; then
-			freq=''
-			if [[ $line =~ $freq_regex ]]; then
-				freq="$line"
-			elif [[ "$line" == "--" ]]; then
-				buf="$buf,$freq,,,,,,,,,,"
-				skip=1
-				end=1
-			elif [[ $line =~ ([0-9]+).Points ]]; then
-				skip=1
-				end=1
-				buf="$buf,$freq,,,,,,,,,,${BASH_REMATCH[1]}"
-			fi
-			capture_freq=''
-			[ -n "$end" ] && do_end && continue
-			buf="$buf,$freq,,,,,,,,,"
-		fi
-
-		if [[ "$line" == "--" ]]; then
-			buf="$buf,"
-			skip=1
-			do_end && continue
-		elif [[ $line =~ ([0-9]+).Points ]]; then
-			buf="$buf,${BASH_REMATCH[1]}"
-			skip=1
-			do_end && continue
-		fi
-		if [[ "$line" == *"GHz"* ]]; then
-			capture_freq=1
+		if [ $skip -gt 0 ]; then
+			((skip-=1))
 			continue
 		fi
-		[[ "$line" == *"Federal"* ]] && continue
-		[[ "$line" == *"Academy"* ]] && continue
-		[[ "$line" == *"Administration"* ]] && continue
-	fi
-done < $dump
+
+		if [ $started -eq 0 ]; then
+			buf="$line"
+			((started+=1))
+		else
+			if [ -n "$capture_freq" ]; then
+				freq=''
+				if [[ $line =~ $freq_regex ]]; then
+					freq="$line"
+				elif [[ "$line" == "--" ]]; then
+					buf="$buf,$freq,,,,,,,,,,"
+					skip=1
+					end=1
+				elif [[ $line =~ ([0-9]+).Points ]]; then
+					skip=1
+					end=1
+					buf="$buf,$freq,,,,,,,,,,${BASH_REMATCH[1]}"
+				fi
+				capture_freq=''
+				[ -n "$end" ] && do_end && continue
+				buf="$buf,$freq,,,,,,,,,"
+			fi
+
+			if [[ "$line" == "--" ]]; then
+				buf="$buf,"
+				skip=1
+				do_end && continue
+			elif [[ $line =~ ([0-9]+).Points ]]; then
+				buf="$buf,${BASH_REMATCH[1]}"
+				skip=1
+				do_end && continue
+			fi
+			if [[ "$line" == *"GHz"* ]]; then
+				capture_freq=1
+				continue
+			fi
+			[[ "$line" == *"Federal"* ]] && continue
+			[[ "$line" == *"Academy"* ]] && continue
+			[[ "$line" == *"Administration"* ]] && continue
+		fi
+	done < $dump
+	write_csv_footer
+}
+
+heat=''
+
+do_login_mode()
+{
+	# read the auth file to get user/pass
+	user=$(grep -E "^user:" $authfile | cut -d: -f2-)
+	pass=$(grep -E "^pass:" $authfile | cut -d: -f2-)
+
+	# login to mgp, and get the auth cookie
+	curl -s --cookie-jar cjar --output /dev/null $login_url
+	curl -s --cookie cjar --cookie-jar cjar \
+		--data "LoginForm[username]=$user" \
+		--data "LoginForm[password]=$pass" \
+		--data 'yt0=Log in' \
+		--location \
+		--output $dump $login_url
+
+	# download the race dump
+	curl -s --cookie cjar --cookie-jar cjar --output race.html --location $url
+	w3m -dump ./race.html > $dump
+	[ -s $dump ] || die "error reading the race page"
+
+	write_csv_headers
+	while IFS= read -r line; do
+		[ -z "$line" ] && continue
+		[[ "$line" == "Round #1" ]] && parse=1 && continue # start parsing
+		[[ "$line" == "Round #2" ]] && parse='' # stop when reached
+		[[ "$line" == "Pilots Racing" ]] && parse='' # stop when reached
+		[ -n "$parse" ] || continue
+		[[ "$line" == *"All Races"* ]] && continue
+		echo $line
+
+		if [[ "$line" =~ 1..Heat.([0-9]+) ]]; then
+			heat="${BASH_REMATCH[1]}"
+			echo " === H E A T  $heat ===" >> $csv
+		fi
+		if [ -n "$heat" ]; then
+			if [[ $line =~ $pilot_regex ]]; then
+				pilot="${BASH_REMATCH[1]}"
+				pilot=$(cut -d' ' -f1 <<< $pilot)
+				[[ "$pilot" == "[EMPTY]" ]] && pilot=''
+			fi
+			if [[ $line =~ $freq_regex ]]; then
+				freq="$line"
+				echo "$pilot,$freq,,,,,,,,,," >> $csv
+			fi
+		fi
+	done < $dump
+	write_csv_footer
+}
+
+## main ##
+
+[ -s "$authfile" ] && do_login_mode || do_public_mode
+
+[ -s $csv ] || die "error producing the CSV file"
 
 cat > convert.php <<EOF
 <?php
@@ -137,5 +222,4 @@ echo "\n</table></body></html>";
 EOF
 
 php -f convert.php > $race.html
-rm convert.php
-rm $dump
+cleanup
